f59933a0dbba7cfa3887c78670fcab60e66a3071
rananth99 <rananth108@gmail.com>
2023-07-16 15:43:55 -0400

    Added Files

---
+++ notes/views.py
@@ -0,0 +1,46 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+from .models import Notes
+from django.http import Http404
+from django.views.generic import UpdateView,CreateView, DetailView, ListView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import NotesForm
+# Create your views here.
+
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
+    template_name = 'notes/notes_detail.html'
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
